7bbb86ea15ebe42b36f4534d2c13a1bd399af985
VPeleven <vaskapiruzashvili@gmail.com>
2021-03-13 10:03:47 +0400

    demo

---
+++ app.js
@@ -0,0 +1,16 @@
+var headerSlider = new Swiper('.header-slider', {
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    
+    }
+  });
+
+  var lectorsSlider = new Swiper('.lectors-slider', {
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+      clickable: true
+    }
+  });
+
