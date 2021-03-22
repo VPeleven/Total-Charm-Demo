f574d31d4eeec3766fa592433fbbc996bdbb0b99
VPeleven <vaskapiruzashvili@gmail.com>
2021-03-22 11:34:12 +0400

    Demo Version

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var headerSlider = new Swiper('.header-slider', {
     pagination: {
       el: '.swiper-pagination',
       dynamicBullets: true,
-    
-    }
+    },
+    autoplay: {
+      delay: 3000,
+    },
   });
 
   var lectorsSlider = new Swiper('.lectors-slider', {
@@ -12,5 +14,25 @@ var headerSlider = new Swiper('.header-slider', {
       dynamicBullets: true,
       clickable: true
     }
+  });
+
+  var aboutSlider = new Swiper('.about-us-slider', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+    },
+  });
+  var masterClass = new Swiper('.master-class-swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
   });
 
